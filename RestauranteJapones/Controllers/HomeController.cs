92f47551e2bf7300e7c03dc361aa0eb15a18273f
Caetano <caetano.rolando@gmail.com>
2024-12-16 01:31:13 -0300

    Primer commit, cambiar la apariencia un poco, agregar los apartados Inicio, Sucursales, Menu.

--- RestauranteJapones/Controllers/HomeController.cs
+++ RestauranteJapones/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace RestauranteJapones.Controllers
         {
             return View();
         }
+        public IActionResult Sucursales()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
